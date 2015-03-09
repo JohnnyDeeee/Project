0dcd66d764bcfd3363c8f32e0c9a05bf7a579902
John Doe <gamefake@live.nl>
2015-03-09 15:53:13 +0100

    Created login screen with fields

--- Lode Runner Personal/eclipse/Lode Runner/src/javagame/Menu.java
+++ Lode Runner Personal/eclipse/Lode Runner/src/javagame/Menu.java
@@ -2,52 +2,110 @@ package javagame;
 
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
+import org.newdawn.slick.font.effects.ColorEffect;
+import org.newdawn.slick.gui.PasswordTextField;
+import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.*;
 
 public class Menu extends BasicGameState{
 	
-	public String mouse = "Get yo mouse in this screen!";
-	Image menuBackground;
+	private Image menuBackground;
+	
+	private TextField login;
+	private PasswordTextField pass;
+	
+	private String username;
+	private String password;
+	
+	private UnicodeFont uiFont;
+	private Color uiBlue = new Color(72,104,247);
+	private Color uiRed = new Color(176,32,32);
 	
 	public Menu(int state){
 		
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
+		//disable fps meter
 		gc.setShowFPS(false);
 		
+		//Loading things for font
+		uiFont = new UnicodeFont("fonts/MINECRAFTIA-REGULAR.TTF", 22, true, false);
+		uiFont.addAsciiGlyphs();
+		uiFont.getEffects().add(new ColorEffect());
+		uiFont.loadGlyphs();
+		
+		//background
 		menuBackground = new Image("res/intro.png");
+		
+		//TextFields
+		login = new TextField(gc, uiFont, 400, 360, 250, 30);
+		login.setBackgroundColor(Color.darkGray);
+		login.setBorderColor(uiBlue);
+		login.setTextColor(uiRed);
+		login.setFocus(true);
+		
+		pass = new PasswordTextField(gc, uiFont, 400, login.getY() + 70, 250, 30);
+		pass.setBackgroundColor(Color.darkGray);
+		pass.setBorderColor(uiBlue);
+		pass.setTextColor(uiRed);
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+		//background
 		g.setBackground(Color.lightGray);
-		
 		menuBackground.draw(0, 0, gc.getWidth(), gc.getHeight(), Color.white);
-		g.drawString(mouse, 300, 530); //graphic 0,0 is in top left corner
+		
+		//Textfields and buttons
+		uiFont.drawString(login.getX(), login.getY() - 30, "Username:", uiBlue);
+		login.render(gc, g);
+		uiFont.drawString(pass.getX(), pass.getY() - 30, "Password:", uiBlue);
+		pass.render(gc, g);
+		uiFont.drawString(pass.getX(), pass.getY() + 60, "Press ENTER to login", uiRed);
+		
+		//g.drawString(mouse, 300, 530); //graphic 0,0 is in top left corner
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{		
-		//Keyboard/Mouse input
+		//Keyboard input
 		Input input = gc.getInput();
-		int xpos = Mouse.getX(); //mouse 0,0 is in bottom left corner
-		int ypos = Mouse.getY();
 
+		//Action keys in TextFields
+		if (login.hasFocus()){
+			if (input.isKeyPressed(Input.KEY_ENTER)){
+				pass.setFocus(true);
+			}
+			else if (input.isKeyPressed(Input.KEY_DOWN)){
+				pass.setFocus(true);
+			}
+			else if (input.isKeyPressed(Input.KEY_TAB)){
+				pass.setFocus(true);
+			}
+		}
+		else if (pass.hasFocus()){
+			if (input.isKeyPressed(Input.KEY_ENTER)){
+				saveUser(login.getText(), pass.getText());
+				System.out.println(String.format("Username: " + username + "%nPassword: " + password));
+			}
+			else if (input.isKeyPressed(Input.KEY_UP)){
+				login.setFocus(true);
+			}
+		}
 		
-		//Mouse Tracking
-		if (Mouse.isInsideWindow()){
-			mouse = "X: " + xpos + " Y: " + ypos;
-			if (input.isMousePressed(0)){ sbg.enterState(1); } //change state to play
-		}
-		else
-		{
-			mouse = "Get yo mouse in this screen!";
-		}
-		if (input.isKeyPressed(Input.KEY_O)){ sbg.enterState(2); } //change state to options
-		if (input.isKeyPressed(Input.KEY_C)){ sbg.enterState(3); } //change state to credits
-		if (input.isKeyPressed(Input.KEY_S)){ sbg.enterState(4); } //change state to score
+		
+		//if (input.isKeyPressed(Input.KEY_O)){ sbg.enterState(2); } //change state to options
+		//if (input.isKeyPressed(Input.KEY_C)){ sbg.enterState(3); } //change state to credits
+		//if (input.isKeyPressed(Input.KEY_S)){ sbg.enterState(4); } //change state to score
+		
 	}
 	
 	public int getID(){
 		return 0;
+	}
+	
+	public void saveUser(String username, String password){
+		this.username = username;
+		this.password = password;
 	}
 }
